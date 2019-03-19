1b8137579c19fd5f3d9c89a0251edddec48bb8fa
xthelavr <23312691+xthelavr@users.noreply.github.com>
2019-03-19 20:43:36 +0300

    update license and links

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => (
   <footer className="text-center mt-5 mb-5">
     <h6>
-      <a href="https://github.com/spacelavr/" target="_blank" rel="noopener noreferrer">
-        spacelavr
+      <a href="https://github.com/xthelavr/" target="_blank" rel="noopener noreferrer">
+        xthelavr
       </a>
     </h6>
 
